224c7b17887f03d2f4fa18ad268c999cbd0e33af
vegura <annadominie@gmail.com>
2023-04-02 19:58:59 +0300

    [RESOURCE-PROCESSOR] - Create basic structure

---
+++ resource-processor/src/test/java/me/vegura/resourceprocessor/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vegura.resourceprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
