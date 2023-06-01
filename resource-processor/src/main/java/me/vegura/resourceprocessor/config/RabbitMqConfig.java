814f5beb7fb16fbe3c6ecdb9cf13f61d5002a3ab
vegura <annadominie@gmail.com>
2023-06-01 11:15:41 +0300

    [TECH] - Update configs for RabbitMq connection

--- resource-processor/src/main/java/me/vegura/resourceprocessor/config/RabbitMqConfig.java
+++ resource-processor/src/main/java/me/vegura/resourceprocessor/config/RabbitMqConfig.java
@@ -1,7 +1,11 @@
 package me.vegura.resourceprocessor.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
